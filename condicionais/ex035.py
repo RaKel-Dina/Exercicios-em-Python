56b480b3818f64d46d310892b9cf30ee3bb9e43e
Raquel Cordeiro <raquelcordeiro890@gmail.com>
2024-07-31 23:21:51 -0300

    fiz o restante dos exercícios de estruturas condicionais
    Fim dos exercícios de estruturas condicionais do Mundo 1 de Python do Curso em Vídeo.

---
+++ condicionais/ex035.py
@@ -0,0 +1,15 @@
+#Desenvolva um programa que leia o comprimento de três retas e diga ao
+# usuário se elas podem ou não formar um triângulo.
+r1 = float(input('primeira reta:'))
+r2 = float(input('segunda reta: '))
+r3 = float(input('terceira reta: '))
+if (r1 + r2) > r3:
+    print('Pode formar triangulo.')
+else:
+    print('Não pode formar triângulo.')
+print()
+
+
+
+#print(f'{reta_maior} é a reta maior.')
+
